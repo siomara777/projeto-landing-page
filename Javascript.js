3f0f174ea05b07edc2c3248a8da683558d45cd02
siomara777 <mix.total.p.n@gmail.com>
2023-05-05 22:55:53 -0300

    [estilo] apliquei a responsividade nas secoes

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+
+const btn = document.querySelector(".botao button")
+
+
+btn.addEventListener("click", ()=>{
+
+const inputs = document.querySelectorAll(".inputUser")
+inputs.forEach((input)=>{
+input.value=""
+
+})
+
+})
